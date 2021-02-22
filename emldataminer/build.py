d7c2d5111bcff14c218e89a764b5dbe120fbfdb3
Yifferpi <theotheen.bm@gmail.com>
2021-02-22 17:34:12 +0100

    decent result, now put in csv

--- emldataminer/build.py
+++ emldataminer/build.py
@@ -12,31 +12,40 @@ def parseEmail(filename):
     email = parser.BytesParser(policy=policy.default).parse(f)
     f.close()
     line = email.get("From")
+    
+    name, mail = ("", "")
 
-    res = re.search("<.*>", line)
-    if res is not None:
-        print(res.group(0)[1:-1].strip())
     res = re.search("^.*<", line)
-
     if res is not None:
         tmp = res.group(0)[0:-1]
         res = re.search("\".*\"", tmp)
         if res is not None:
-            print(res.group(0)[1:-1].strip())
+            name = res.group(0)[1:-1].strip()
         else:
-            print(tmp.strip())
-    print()
+            name = tmp.strip()
 
-    
+    res = re.search("<.*>", line)
+    if res is not None:
+        mail = res.group(0)[1:-1].strip()
+
+    return (name, mail)
+
+def printList(l):
+    for e in l:
+        print("{:30.30}{}".format(*e))
 
 def main():
     path = sys.argv[1]
-    
+    l = list()    
     for f in os.listdir(path):
         if f.endswith(".eml"):
-            parseEmail(path + f)
-
-
+            l.append(parseEmail(path + f))
+    x = len(l) 
+    l = list(set(l))
+    y = len(l)
+    printList(l)
+    print(x)
+    print(y)
 
 
 if __name__ == "__main__":
