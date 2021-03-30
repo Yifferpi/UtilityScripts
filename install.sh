47c8e94f73fae4710c86b02c61c9b80f0084daec
Yifferpi <theotheen.bm@gmail.com>
2021-03-30 13:32:12 +0200

    adjusting install script

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ echo $path
 
 cd /home/$USER/bin
 
-ln -s pdfscrape $path/pdfscrape
-ln -s jpg2pdf $path/jpg2pdf
-ln -s arrange-screens $path/arrange-screens
-ln -s mkmermaid $path/mkmermaid
+ln -s $path/pdfscrape pdfscrape
+ln -s $path/jpg2pdf jpg2pdf
+ln -s $path/arrange-screens arrange-screens
+ln -s $path/mkmermaid mkmermaid
 
 
 
