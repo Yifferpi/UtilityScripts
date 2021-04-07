73ae595731eba4782127aa0a11e64470e0d8af97
Yifferpi <theotheen.bm@gmail.com>
2021-04-07 12:05:13 +0200

    modified install script

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
+# Make sure target exists and is on execution path
+installfolder="/home/$USER/bin"
+onpath=`echo $PATH | grep -o "$installfolder"`
+if [-d "$installfolder" -a "$onpath" == "" ]; then
+    echo "Make sure '~/bin' is on your execution path"
+    exit 1
+fi
 
+#find gitrepo path which the skript links to 
 path=$(dirname $(readlink -f $0))
-echo $path
 
-cd /home/$USER/bin
-
+cd "$installfolder"
 ln -s $path/pdfscrape pdfscrape
 ln -s $path/jpg2pdf jpg2pdf
 ln -s $path/arrange-screens arrange-screens
