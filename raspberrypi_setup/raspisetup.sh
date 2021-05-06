5c5bc420a0fd421ae0ae9760c03a1fad5195b7a7
Basil Ottinger <basilottinger@gmail.com>
2021-05-06 16:42:00 +0200

    improved raspi-setup script

--- raspberrypi_setup/raspisetup.sh
+++ raspberrypi_setup/raspisetup.sh
@@ -1,19 +1,31 @@
 #! /bin/bash
 
+if [ $(whoami) != "root" ]; then
+    echo "Are you root?"
+    exit 1
+fi
+
+if [ $# -eq 0 ]; then
+    echo "two args required: first: image, second: disk"
+    exit 1
+fi
+
+
 #Flash image given by first arg
-dd bs=4M if=$PWD/$1 of=/dev/sda conv=fsync
+dd bs=4M if="$PWD/$1" of="/dev/sde" conv=fsync
 echo "Flash finished"
 
 # mount boot partition
 mkdir /mnt/raspi_boot
-mount /dev/sda1 /mnt/raspi_boot
-
+mount "/dev/sde1" /mnt/raspi_boot
+sleep 0.5
 #enable ssh
 touch /mnt/raspi_boot/ssh
-touch /mnt/raspi_boot/wpa-supplicant.conf
+touch /mnt/raspi_boot/wpa_supplicant.conf
 #wifi config
-cat wificonfig.secret > /mnt/raspi_boot/wpa-supplicant.conf
+cat wificonfig.secret > /mnt/raspi_boot/wpa_supplicant.conf
 
 umount /mnt/raspi_boot
+sleep 0.5
 rmdir /mnt/raspi_boot
 
