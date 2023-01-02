49f381c5b2d1179e66f5d5522015438bee653441
Yifferpi <yifferpi@yifferpi.ch>
2023-01-03 00:02:55 +0100

    added new script for finding gitrepos with pending changes

--- install.sh
+++ install.sh
@@ -1,22 +1,41 @@
 #! /bin/bash
 
-# Make sure target exists and is on execution path
+# set target folder where links should be created
 installfolder="/home/$USER/bin"
+
+# Make sure target folder exists
+if [ ! -d "$installfolder" ]; then
+    echo "'$installfolder' does not exist"
+    exit 1
+fi
+# Make sure target folder is on path
 onpath=`echo $PATH | grep -o "$installfolder"`
-if [-d "$installfolder" -a "$onpath" == "" ]; then
-    echo "Make sure '~/bin' is on your execution path"
+if [ -d "$installfolder" -a "$onpath" == "" ]; then
+    echo "Make sure '$1' is on your execution path"
     exit 1
 fi
 
-#find gitrepo path which the skript links to 
+# Find current path (location of this repo)
 path=$(dirname $(readlink -f $0))
 
-cd "$installfolder"
-ln -s $path/pdfscrape pdfscrape
-ln -s $path/jpg2pdf jpg2pdf
-ln -s $path/arrange-screens arrange-screens
-ln -s $path/mkmermaid mkmermaid
-ln -s $path/genpw genpw
+function link() {
+    if [ -L "$1" ]; then
+        echo "symlink $1 exists, overwriting.."
+        rm $1
+        ln -s $path/$1 $1
+    elif [ -f "$1" ]; then
+        echo "file with name '$1' already exists, skipping.."
+    else
+        echo "creating symlink for $1"
+        ln -s $path/$1 $1
+    fi
+}
 
+cd "$installfolder"
+link "pdfscrape"
+link "gitrepos2push"
+link "jpg2pdf"
+link "arrange-screens"
+link "mkmermaid"
 
 
